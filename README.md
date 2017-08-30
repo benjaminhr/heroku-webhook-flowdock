d58a8365e41ae30f61447944892cbdc1e694dc23
benjaminr177 <unoceptik@gmail.com>
2017-08-30 11:46:38 +0300

    Everything dumped

---
+++ README.md
@@ -0,0 +1,9 @@
+# Heroku webhook + Flowdock notifications template
+
+- **Things you will need to do:**
+  - follow this [tutorial](https://devcenter.heroku.com/articles/app-webhooks#receiving-webhooks) to setup heroku webhook
+  - follow this [tutorial](https://www.flowdock.com/api/integration-getting-started) to setup Flowdock notifications
+  - Add Flowdock curl command with your own keys, etc. to **app.rb**
+
+- **TODO:**
+  - Format ---.com/webhook data, right now it is just being thrown there
---
+++ app.rb
@@ -0,0 +1,60 @@
+require 'sinatra'
+
+get '/' do
+  "<h3>ignore</h3>"
+end
+
+# global var - used in index.html.erb
+$output = []
+
+post '/webhook' do
+  status 200
+
+  # heroku webhook authentication
+  def valid_signature?(request, webhook_secret)
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(
+      OpenSSL::Digest.new('sha256'),
+      webhook_secret,
+      request.raw_post
+    )).strip
+    heroku_hmac = request.headers['Heroku-Webhook-Hmac-SHA256']
+
+    heroku_hmac && Rack::Utils.secure_compare(calculated_hmac, heroku_hmac)
+  end
+
+  # picking out data from heroku post request
+  json_data = JSON.parse(request.body.read.to_s)
+  $output << json_data
+  $output.join(", ")
+
+  heroku_app_name = json_data["data"]["app"]["name"]
+  heroku_timestamp = json_data["data"]["created_at"]
+  heroku_description = json_data["data"]["description"]
+  heroku_email = json_data["actor"]["email"]
+
+  # command to post message to flowdock with unique flow token
+  %x(curl -i -X POST -H "Content-Type: application/json" -d '{
+        "flow_token": "78a27f6405d80ab8a7fa8b33a770114f",
+        "event": "activity",
+        "author": {
+          "name": "#{heroku_app_name}",
+          "avatar": "https://a.slack-edge.com/bfaba/img/api/hosting_heroku.png"
+        },
+        "title": "#{heroku_description}",
+        "external_thread_id": "#{heroku_timestamp}",
+        "thread": {
+          "title": "Heroku",
+          "body": "none",
+          "external_url": "https://sinatra-webhook.herokuapp.com/webhook",
+          "status": {
+            "color": "grey",
+            "value": "CHANGE"
+          }
+        }
+      }' https://api.flowdock.com/messages)
+
+end
+
+get '/webhook' do
+  erb :'index.html'
+end
